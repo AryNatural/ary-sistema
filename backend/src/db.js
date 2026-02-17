3c70fab36dc8b98048eee4483b9e553d12063ca9
AryNatural <publicidadbi.cr@gmail.com>
2026-02-17 15:49:05 -0600

    Add files via upload

---
+++ backend/src/db.js
@@ -0,0 +1,20 @@
+﻿import { Sequelize } from "sequelize";
+
+
+const DATABASE_URL = process.env.DATABASE_URL;
+
+
+if (!DATABASE_URL) {
+  throw new Error("Falta DATABASE_URL en variables de entorno (Railway).");
+}
+
+
+export const sequelize = new Sequelize(DATABASE_URL, {
+  dialect: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: process.env.NODE_ENV === "production"
+      ? { require: true, rejectUnauthorized: false }
+      : false
+  }
+});
