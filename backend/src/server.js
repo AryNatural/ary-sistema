287bea3ea8979925385564966edb899da8a764e1
AryNatural <publicidadbi.cr@gmail.com>
2026-02-17 15:29:45 -0600

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
+import "dotenv/config";
 import app from "./app.js";
 
-const PORT = Number(process.env.PORT || 5000);
+const PORT = Number(process.env.PORT || 8080);
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`API running on port ${PORT}`);
