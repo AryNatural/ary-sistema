67f9f8096fb47edf08ab30a2d8c90a73e1d4020c
AryNatural <publicidadbi.cr@gmail.com>
2026-02-17 22:09:30 -0600

    Update Product.js

--- backend/src/models/Product.js
+++ backend/src/models/Product.js
@@ -1,16 +1,21 @@
-﻿import { DataTypes } from "sequelize";
+import { DataTypes } from "sequelize";
 import { sequelize } from "../db.js";
-
 
 export const Product = sequelize.define(
   "Product",
   {
     id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
+
     name: { type: DataTypes.STRING, allowNull: false },
     sku: { type: DataTypes.STRING, allowNull: true, unique: true },
+
     price: { type: DataTypes.DECIMAL(10, 2), allowNull: false, defaultValue: 0 },
     stock: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0 },
-    active: { type: DataTypes.BOOLEAN, defaultValue: true }
+
+    active: { type: DataTypes.BOOLEAN, defaultValue: true },
   },
-  { tableName: "products", timestamps: true }
+  {
+    tableName: "products",
+    timestamps: true,
+  }
 );
