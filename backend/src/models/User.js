85ab53a680efbdc564d190c4b61efcc68e9bbb62
AryNatural <publicidadbi.cr@gmail.com>
2026-02-23 15:38:31 -0600

    Update User.js

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -9,26 +9,12 @@ export const User = sequelize.define(
       defaultValue: DataTypes.UUIDV4,
       primaryKey: true,
     },
-    nombre: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    password_hash: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    rol: {
-      type: DataTypes.ENUM("Admin", "Ventas", "Bodega", "Contabilidad", "Producción"),
-      defaultValue: "Producción",
-    },
+    nombre: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, allowNull: false, unique: true },
+    passwordHash: { type: DataTypes.STRING, allowNull: false },
+    rol: { type: DataTypes.STRING, allowNull: false, defaultValue: "Producción" },
   },
   {
     tableName: "users",
-    timestamps: true,
   }
 );
