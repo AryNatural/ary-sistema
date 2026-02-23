0bc0a0316b6b32f1b918973a916aa06872f82fed
AryNatural <publicidadbi.cr@gmail.com>
2026-02-23 10:50:13 -0600

    Add files via upload

---
+++ backend/src/models/User.js
@@ -0,0 +1,35 @@
+﻿import { DataTypes } from "sequelize";
+import { sequelize } from "../db.js";
+
+
+export const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password_hash: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    role: {
+      type: DataTypes.ENUM("Admin", "Ventas", "Bodega", "Contabilidad"),
+      allowNull: false,
+      defaultValue: "Ventas"
+    }
+  },
+  {
+    tableName: "users"
+  }
+);
