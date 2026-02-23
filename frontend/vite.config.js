da244c048e2749a4553f32473162e2158645c7eb
AryNatural <publicidadbi.cr@gmail.com>
2026-02-23 10:02:09 -0600

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-﻿import { defineConfig } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  preview: {
+    allowedHosts: [
+      "wholesome-passion-production-c125.up.railway.app"
+    ]
+  }
 });
